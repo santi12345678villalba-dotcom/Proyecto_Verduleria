e2bea04c60b503437aaf4b57cb9847887fbb6fa4
Cesarrvill <cesarrvill21@gmail.com>
2026-08-18 20:21:47 -0300

    vista mobil de las cajas de listado

--- dominio/controller/CatalogoABM.js
+++ dominio/controller/CatalogoABM.js
@@ -69,16 +69,8 @@ const mostrarComoLista = () => {
         html += '<td>' + catalogos[i].nombre + '</td>';
         html += '<td>' + catalogos[i].precio + '</td>';
         html += '<td>' + catalogos[i].stock + '</td>';
-
-        html += '<td class="acciones">';
-        html += '</td>';
-
         html += '</tr>';
     }
-
-    html += '</tbody>';
-    html += '</table>';
-
     contenedor.innerHTML = html;
 };
 
